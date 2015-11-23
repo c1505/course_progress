aaf0fac1847b62ce9f5a0666cbcc8799e5237ca4
c1505 <coreym247@gmail.com>
2015-11-23 06:00:15 +0000

    added bootstrap

--- app/assets/javascripts/sub_sections.js
+++ app/assets/javascripts/sub_sections.js
@@ -1,30 +1,27 @@
-function prepareList() {
-    $('#expList').find('li:has(ul)')
-    .click( function(event) {
-        if (this == event.target) {
-            $(this).toggleClass('expanded');
-            $(this).children('ul').toggle('medium');
-        }
-        return false;
-    })
-    .addClass('collapsed')
-    .children('ul').hide();
-
-    //Create the button funtionality
-    $('#expandList')
-    .unbind('click')
-    .click( function() {
-        $('.collapsed').addClass('expanded');
-        $('.collapsed').children().show('medium');
-    })
-    $('#collapseList')
-    .unbind('click')
-    .click( function() {
-        $('.collapsed').removeClass('expanded');
-        $('.collapsed').children().hide('medium');
-    })
-    
-};
+$(function prepareList() {
+	$('#expList').find('li:has(ul)').unbind('click').click(function(event) {
+		if(this == event.target) {
+			$(this).toggleClass('expanded');
+			$(this).children('ul').toggle('medium');
+		}
+		return false;
+	}).addClass('collapsed').removeClass('expanded').children('ul').hide();
+ 
+	//Hack to add links inside the cv
+	$('#expList a').unbind('click').click(function() {
+		window.open($(this).attr('href'));
+		return false;
+	});
+	//Create the button functionality
+	$('#expandList').unbind('click').click(function() {
+		$('.collapsed').addClass('expanded');
+		$('.collapsed').children().show('medium');
+	})
+	$('#collapseList').unbind('click').click(function() {
+		$('.collapsed').removeClass('expanded');
+		$('.collapsed').children().hide('medium');
+	})
+});
 
 
 /**************************************************************/
